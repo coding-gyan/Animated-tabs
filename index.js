e8c49a24a1b9df7337d2c854b541f6e13d000f28
shubham44-code <shubham162001@gmail.com>
2023-04-22 18:51:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const pointer = document.querySelector(".pointer");
+const content = document.querySelectorAll(".content");
+const tab = document.querySelectorAll(".tab i");
+
+tab.forEach((e) => {
+  e.addEventListener("click", (_) => {
+    tab.forEach((e) => e.classList.remove("active"));
+    content.forEach((e) => e.classList.remove("active"));
+
+    let currentTab = _.target.getAttribute("data-tab");
+    let currentContent = document.querySelector(
+      `.content[data-content="${currentTab}"]`
+    );
+    currentContent.classList.add("active");
+    pointer.style.transform = `translateX(${58 * currentTab}px)`;
+    _.target.classList.add("active");
+  });
+});
